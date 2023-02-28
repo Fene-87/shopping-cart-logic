034a3e513648027dbc2fb75beef9c256d6501589
Fene-87 <fenekayas87@gmail.com>
2023-02-28 10:34:19 +0300

    Implement clear items modal

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
+import { useDispatch, useSelector } from "react-redux";
+import { calculateTotals } from "./features/cart/cartSlice";
+import { useEffect } from "react";
+import Modal from "./components/Modal";
 
 function App() {
+  const {cartItems} = useSelector((store) => store.cart);
+  const {isOpen} = useSelector((store) => store.modal);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
+
   return (
     <main>
+      {isOpen && <Modal />}
       <Navbar />
       <CartContainer />
     </main>
